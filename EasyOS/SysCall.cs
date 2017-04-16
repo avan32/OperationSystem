c82fa6e93df50273a7598b84990a936911014eca
avan32 <avan_32@mail.ru>
2017-04-16 12:48:49 +0400

    Add files via upload

---
+++ EasyOS/SysCall.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+partial class FileSystem  //must be namespace EasyOS
+{
+    class SysCall
+    {
+        public static void Open(string path, FileSystem.FileAccess access)
+        {
+
+
+            string[] stringDir = new string[3];//must writeing 
+            string fileName; //must writeing
+
+            for (int i = 0; i < stringDir.Length; i++)
+            {
+                int currentDirId = InodeDirNameAndIDTable.GetInodeDirID(stringDir[i]);
+                //seacrhc now
+
+                InodeDir d = new InodeDir((byte)currentDirId);
+                
+
+
+            }
+        }
+
+       
+
+        public static void Creat(string path) { }
+        public static void Write() { }
+        public static void Read() { }
+    }
+}
